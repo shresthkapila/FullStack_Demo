6a20c45f531672c953181e3ceace7a93c7d3cf31
Shresth Kapila <49010193+shresthkapila@users.noreply.github.com>
2020-06-11 13:56:24 -0700

    nodejs demo learned

--- index.js
+++ index.js
@@ -2,9 +2,32 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => res.render('pages/index'));
+app.get('/database', (req,res) => {
+  var data = {results: [2,3,4,5,6]};
+  res.render('pages/db',data);
+});
+app.post('/adduser',(req,res)=>{
+  console.log("post request for /adduser");
+  var uname = req.body.uname;
+  var age = req.body.age;
+  res.send(`username: ${uname}, age: ${age}`);
+});
+app.get('/users/:id', (req,res)=> {
+  var uid = req.params.id
+  console.log(req.params.id);
+  // search the database using uid
+  res.send("got it");
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
---
+++ public/advanced.js
@@ -0,0 +1,34 @@
+//js variable scope
+// global
+// function - var
+// block - let
+
+// function myfunc() {
+//     v =  "myfunc_var";  //global
+//     let le = "myfunc_let" //not visible outside function
+
+//     console.log(v);
+//     console.log(le);
+
+//     for (let i=0; i < 10 ; i++) {
+//         console.log(i);
+//     }
+//     //console.log(i);
+// }
+
+// myfunc();
+// console.log(v);
+
+/*
+var cb1 = ()=>{console.log("cb1");}
+var cb2 = ()=>{console.log("cb2");}
+
+console.log("start...");
+setTimeout(cb1,1000);
+setTimeout(cb2,500);
+console.log("end...");
+*/
+
+
+
+
